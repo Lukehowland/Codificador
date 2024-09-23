317ca736f6e784f7a83c32a6d1e7d3f91227165f
Lukehowland <122487510+Lukehowland@users.noreply.github.com>
2024-09-23 09:27:16 -0400

    Add files via upload

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LukeCodificador.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult SobreElInge()
+        {
+            return View();
+        }
+    }
+}
